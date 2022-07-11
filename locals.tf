f96b1c5176c192b0b0f4e95fc890988e203e3dd4
EliMeitner <eli.meitner@toluna.com>
2022-07-11 14:28:09 +0300

    Update .gitignore, LICENSE, and 12 more files...

---
+++ locals.tf
@@ -0,0 +1,49 @@
+locals {
+  # ECS SG rules
+  ecs_security_group_rules = {
+    ingress_http = {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      type        = "ingress"
+      cidr_blocks = ["0.0.0.0/0"] // We are protected by the VPC here so it is fine
+    }
+
+    ingress_all = {
+      from_port = 0
+      to_port   = 0
+      protocol  = "-1"
+      type      = "ingress"
+      self      = true
+    }
+
+    egress_all = {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      type        = "egress"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress_vpce = {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      type            = "egress"
+      prefix_list_ids = ["${data.aws_prefix_list.private_s3.id}"]
+    }
+  }
+
+  dockerLabels                  = jsonencode(var.dockerLabels)
+  app_container_environment     = jsonencode(var.app_container_environment)
+  envoy_dockerLabels            = [ for key in toset(["blue","green"]) :
+    jsonencode(var.envoy_dockerLabels)
+  ]
+  envoy_container_environment   = [ for key in toset(["blue","green"]) :
+    jsonencode(var.envoy_container_environment)
+  ]
+  app_container_secrets         = jsonencode(var.app_container_secrets)
+  datadog_container_secrets     = jsonencode(var.datadog_container_secrets)
+  datadog_container_environment = jsonencode(var.datadog_container_environment)
+
+}
---
+++ variables.tf
@@ -0,0 +1,199 @@
+# General variables for module
+variable "app_name" {
+  description = "app name"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
+}
+
+variable "env_type" {
+  description = "prod || non-prod"
+  type        = string
+}
+
+variable "aws_profile" {
+  description = "profile"
+  type        = string
+}
+
+variable "ecs_service_desired_count" {
+  description = "ecs service desired count"
+  type        = number
+}
+
+variable "ecr_repo_url" {
+  description = "ecr repo url"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs used in Service"
+  type        = list(string)
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "VPC id"
+  type        = string
+}
+
+variable "ecs_security_group_additional_rules" {
+  description = "List of additional security group rules to add to the security group created"
+  type        = any
+  default     = {}
+}
+
+variable "iam_role_additional_policies" {
+  description = "Additional policies to be added to the IAM role"
+  type        = list(string)
+  default     = []
+}
+
+variable "task_definition_cpu" {
+  description = "Task definition CPU"
+  type        = number
+  default     = 2048
+}
+
+variable "task_definition_memory" {
+  description = "Task definition memory"
+  type        = number
+  default     = 4096
+}
+
+# Default container related variables
+variable "app_container_cpu" {
+  description = "Default container cpu"
+  type        = number
+  default     = 2
+}
+
+variable "app_container_memory" {
+  description = "Default container memory"
+  type        = number
+  default     = 4096
+}
+
+variable "app_container_port" {
+  description = "Default container port"
+  type        = number
+  default     = 80
+}
+
+variable "aws_cloudwatch_log_group_name" {
+  description = "Cloud watch log group name"
+  type        = string
+}
+
+variable "app_container_environment" {
+  description = "The environment variables to pass to a container"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "dockerLabels" {
+  description = "A key/value map of labels to add to the container"
+  type        = map(string)
+  default     = {}
+}
+
+variable "envoy_container_environment" {
+  description = "The environment variables to pass to a container"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "envoy_dockerLabels" {
+  description = "A key/value map of labels to add to the container"
+  type        = map(string)
+  default     = {}
+}
+
+variable "app_container_secrets" {
+  description = "The secrets to pass to the app container"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "app_container_image" {
+  description = "App container image"
+  type        = string
+}
+
+# Datadog container related variables
+variable "create_datadog" {
+  description = "Boolean which initiate datadog container creation or not"
+  type        = bool
+  default     = false
+}
+variable "datadog_container_cpu" {
+  description = "Datadog container cpu"
+  type        = number
+  default     = 10
+}
+
+variable "datadog_container_memoryreservation" {
+  description = "Datadog container memory"
+  type        = number
+  default     = 256
+}
+
+variable "datadog_container_port" {
+  description = "Datadog container port"
+  type        = number
+  default     = 8126
+}
+
+variable "datadog_container_name" {
+  description = "Datadog container name"
+  type        = string
+  default     = "datadog_agent"
+}
+
+variable "datadog_container_image" {
+  description = "Datadog container image"
+  type        = string
+  default     = "datadog/agent:latest"
+}
+
+variable "datadog_container_environment" {
+  description = "Datadog container environment variables"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "datadog_container_secrets" {
+  description = "The secrets to pass to the datadog container"
+  type        = list(map(string))
+  default     = []
+}
+
+
+variable "envoy_app_ports" {
+  description = "The app ports for envoy to listen to"
+  type  = string
+  default = "80"
+}
+
+variable "namespace_id" {
+  description = "The app namespace id"
+  type = string
+}
+
+variable "namespace" {
+  description = "The app namespac"
+  type = string
+}
+
+variable "app_mesh_owner" {
+  description = "The mesh owner id"
+  type = string
+}
+
+variable "app_mesh_name" {
+  description = "The mesh name"
+  type = string
+}
