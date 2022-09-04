193a38b4a3f4b1c3038768337f475c2d066b8a06
ShakedYosef <85097819+ShakedYosef@users.noreply.github.com>
2022-09-04 12:50:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+locals {
+  # ECS SG rules
+  security_cidr = split(",", data.aws_ssm_parameter.security_cidr.value)
+
+  dockerLabels                  = jsonencode(var.dockerLabels)
+  app_container_environment     = jsonencode(var.app_container_environment)
+  envoy_dockerLabels            = jsonencode(var.envoy_dockerLabels)
+  envoy_container_environment   = jsonencode(var.envoy_container_environment)
+  app_container_secrets         = jsonencode(var.app_container_secrets)
+  datadog_container_secrets     = jsonencode(var.datadog_container_secrets)
+  datadog_container_environment = jsonencode(var.datadog_container_environment)
+  datadog_dockerLabels          = jsonencode(var.datadog_dockerLabels)
+}
+
+
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "${var.app_name}-${var.env_name}"
 }
